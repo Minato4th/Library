d2a7055865b017a04bbc94130d4fe570d34a0b11
Minato4th <borisko.aleksey@gmail.com>
2017-03-24 17:20:18 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.endava.service.Menu;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/com/endava/entities/Year.java
@@ -0,0 +1,13 @@
+package com.endava.entities;
+
+public class Year extends BookParts {
+    public Year(String part) {
+        super(part);
+    }
+    /*public Year(){
+        setUpdateData(0, "2008");
+        setUpdateData(1, "1998");
+        setUpdateData(2, "2006");
+    }*/
+}
+
