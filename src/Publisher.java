959cec75cb487e6d3141ebb744449f57a47ad17b
Minato4th <borisko.aleksey@gmail.com>
2017-03-24 17:24:29 +0200

    Delete Publisher.java

--- src/Publisher.java
+++
@@ -1,8 +0,0 @@
-
-public class Publisher extends Book{
-    Publisher(){
-        setUpdateData(0, "Scholastic Press");
-        setUpdateData(1, "Scholastic Inc.");
-        setUpdateData(2, "Harper Perennial Modern Classics");
-    }
-}
