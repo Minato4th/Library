6ea2fee2504ad9423088b4f6b74aa12a8fcf9600
Minato4th <borisko.aleksey@gmail.com>
2017-03-24 17:24:35 +0200

    Delete Title.java

--- src/Title.java
+++
@@ -1,9 +0,0 @@
-
-public class Title extends Book {
-
-    Title(){
-        setUpdateData(0, "The Hunger Games");
-        setUpdateData(1, "Harry Potter");
-        setUpdateData(2, "To Kill a Mockingbird");
-    }
-}
