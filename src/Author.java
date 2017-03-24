0bec0c198b43ca75cf9be1bd12eeb5e6a47ab85c
Minato4th <borisko.aleksey@gmail.com>
2017-03-24 17:23:44 +0200

    Delete Author.java

--- src/Author.java
+++
@@ -1,8 +0,0 @@
-
-public class Author extends Book {
-    Author(){
-        setUpdateData(0, "Suzanne Collins");
-        setUpdateData(1, "J.K. Rowling");
-        setUpdateData(2, "Harper Lee ");
-    }
-}
